51c9db3e894a74d2f9fa7cba2de8ce5ca40cb8c7
Kittu <70826024+noob-kittu@users.noreply.github.com>
2021-10-22 02:00:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,6 +63,15 @@ kittu = "this bot is made by kittu"
 
 
 
+@bot.on(events.NewMessage(pattern="^/start (.+)"))
+async def start(event):
+    event.reply(f'''This is A spam Bot, Owned by @{OWNER_USERNAME} and created by @Autichrist \n\nGive Command \n/help to know All commands of this bot''')
+
+
+@bot.on(events.NewMessage(pattern="^/help (.+)"))
+async def help(event):
+    event.reply(f'''Available Commands of bot: \n\n /spam \n /cspam \n /wspam \n /picspam \n /depayspam \n\n\n if you like this bot then please follow this github account -  [FOLLOW ME](https://github.com/noob-kittu)''')
+
 
 
 @bot.on(events.NewMessage(pattern="^/cspam (.+)"))
